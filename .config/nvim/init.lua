af330350a4209add78090ed384d171112f8a8045
Jacob Scott <jake12358@gmail.com>
2024-01-09 12:47:37 +0000

    update nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,8 +1,9 @@
--- Set options that need vimscript
+-- Set options that need vimscript 
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+vim.cmd("set colorcolumn=80,120")
 vim.g.mapleader = " "
 vim.wo.relativenumber = true
 
@@ -20,34 +21,5 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
--- Create vars for plugins
-local plugins = {
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-  {
-    'nvim-telescope/telescope.nvim', tag = '0.1.5',
-    dependencies = { 'nvim-lua/plenary.nvim' }
-  },
-  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}
-}
-local opts = {}
-
--- Setup plugins
-require("lazy").setup(plugins, opts)
-
--- Setup theme
-require("catppuccin").setup({ flavour = "mocha" })
-vim.cmd.colorscheme "catppuccin"
-
--- Setup Telescope
-local builtin = require("telescope.builtin")
-vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-
--- Setup treesitter
-local config = require("nvim-treesitter.configs")
-config.setup({
-  ensure_installed = { "lua", "python", "r" },
-  highlight = { enabled = true },
-  indent = { enabled = true }
-})
-
+-- Set up plugins
+require("lazy").setup("plugins")
---
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,15 @@
+return {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    },
+    config = function() 
+      vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>')
+      vim.keymap.set('n', '<C-m>', ':Neotree close<CR>')
+    end
+}
+
