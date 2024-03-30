e53fe89370930f7b4ea6112694548bd5ab7e4ec6
Jacob Scott <jscott2718@gmail.com>
2024-03-30 15:03:35 +0000

    More messing with themes

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -296,15 +296,6 @@ return {
                         LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
                         MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
 
-                        -- Borderless telescope
-                        TelescopeTitle = { fg = theme.ui.special, bold = true },
-                        TelescopePromptNormal = { bg = theme.ui.bg_p1 },
-                        TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = theme.ui.bg_p1 },
-                        TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m1 },
-                        TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = theme.ui.bg_m1 },
-                        TelescopePreviewNormal = { bg = theme.ui.bg_dim },
-                        TelescopePreviewBorder = { bg = theme.ui.bg_dim, fg = theme.ui.bg_dim },
-
                         -- Dark popup windows
                         Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1, blend = vim.o.pumblend },
                         PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
@@ -314,7 +305,7 @@ return {
                 end,
                 theme = "wave",              -- Load "wave" theme when 'background' option is not set
                 background = {               -- map the value of 'background' option to a theme
-                    dark = "wave",           -- try "dragon" !
+                    dark = "dragon",           -- try "dragon" !
                     light = "lotus"
                 },
             })
