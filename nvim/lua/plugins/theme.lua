c128cdae2c588c49be54f9599159c297182edbd4
Jacob Scott <jscott2718@gmail.com>
2024-04-09 15:43:04 +0100

    use rose pine default theme

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -263,7 +263,7 @@ return {
         name = "rose-pine",
         config = function()
             require("rose-pine").setup({
-                variant = "moon",
+                -- variant = "moon",
                 dim_inactive_windows = true
             })
             vim.cmd[[colorscheme rose-pine]]
