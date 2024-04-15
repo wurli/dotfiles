3e4fbf21fbf07edff6910d24c4e8430fcbf54649
Jacob Scott <jscott2718@gmail.com>
2024-04-15 19:06:56 +0100

    update a bunch of stuff

--- nvim/lua/plugins/r-nvim.lua
+++ nvim/lua/plugins/r-nvim.lua
@@ -1,140 +1,183 @@
-local nvim_rprofile_path = (
-    vim.fn.stdpath("config") .. "/nvim-rprofile.R"
-):gsub("\\", "/")
-
-local r_console_winid = function()
-    local bufno = require("r.term").get_buf_nr()
-    if bufno == nil then
-        return -1
-    else
-        return vim.fn.bufwinid(bufno)
-    end
-end
-
-local r_console_open = function() require("r.run").start_R("R") end
-local r_console_close = function(winid) vim.fn.win_execute(winid, "q") end
-
-local toggle_r_console = function()
-    local winid = r_console_winid()
-    if winid == -1 then
-        r_console_open()
-    else
-        r_console_close(winid)
-    end
-end
-
-local start_r_and_then = function(f)
-    r_console_open()
-
-    -- Should happen very rarely, e.g. if you try to send 
-    -- a line less than a second after opening a script
-    if vim.g.R_Nvim_status < 4 then return end
-
-    -- Repeatedly check to see if R has started - once yes,
-    -- send the line
-    local timer = vim.loop.new_timer()
-    local i = 0
-    timer:start(0, 200, vim.schedule_wrap(function()
-        -- Give up trying to start R after about 10 seconds
-        if vim.g.R_Nvim_status > 6 or i > 50 then
-            timer:close()
-            f()
-        end
-        i = i + 1
-    end))
-end
-
-local send_line = function()
-
-    -- If R is running, open the console if closed and send 
-    -- the line
-    if vim.g.R_Nvim_status > 6 then
-        if r_console_winid() == -1 then r_console_open() end
-        require("r.send").line(true)
-        return
-    end
-
-    start_r_and_then(function() require("r.send").line(true) end)
-end
-
 return {
-    "R-nvim/R.nvim",
-    config = function()
-        -- Create a table with the options to be passed to setup()
-        local opts = {
-            R_args = { "--quiet", "--no-save" },
-            hook = {
-
-                after_R_start = function()
-                    require("r.send").cmd('source("' .. nvim_rprofile_path .. '")')
-                end,
-
-                after_config = function()
-                    -- This function will be called at the FileType event
-                    -- of files supported by R.nvim. This is an
-                    -- opportunity to create mappings local to buffers
-
-                    if vim.o.syntax ~= "rbrowser" then
-                        vim.keymap.set("n", "<Enter>", send_line, { buffer = 0 })
-                        vim.api.nvim_buf_set_keymap(0, "v", "<Enter>", "<Plug>RSendSelection", {})
+        "R-nvim/R.nvim",
+        config = function ()
+            -- Create a table with the options to be passed to setup()
+            local opts = {
+                R_args = {"--quiet", "--no-save"},
+                hook = {
+                    after_config = function ()
+                        -- This function will be called at the FileType event
+                        -- of files supported by R.nvim. This is an
+                        -- opportunity to create mappings local to buffers.
+                        if vim.o.syntax ~= "rbrowser" then
+                            vim.api.nvim_buf_set_keymap(0, "n", "<Enter>", "<Plug>RDSendLine", {})
+                            vim.api.nvim_buf_set_keymap(0, "v", "<Enter>", "<Plug>RSendSelection", {})
+                        end
                     end
-
-                    vim.keymap.set("n", "<localleader>rt", toggle_r_console, { buffer = 0 })
-                    vim.keymap.set("n", "<localleader>rq", "<Plug>RClose", { buffer = 0 })
-                    vim.keymap.set("n", "<localleader>ro", "<Plug>ROBToggle", { buffer = 0 })
-
-                    -- devtools::load_all()
-                    vim.keymap.set(
-                        { "n", "i" }, "<localleader>la",
-                        function()
-                            start_r_and_then(function()
-                                require("r.send").cmd("devtools::load_all()")
-                            end)
-                        end,
-                        { buffer = 0 }
-                    )
-
-                    -- Pipe operator
-                    vim.api.nvim_buf_set_keymap(0, "i", "<C-k>", " |>", {})
-                end,
-            },
-
-            -- Note that on macOS, you need to set the option key as the 'meta'
-            -- key, e.g. in your iterm2 profile, for this to work
-            assign_map = "<M-->",
-
-            -- For some reason gets set to 'Rterm' on windows
-            R_cmd = "R",
-            R_app = "R",
-
-            -- Windows testing
-            -- R_path = "C:\\Program Files\\R\\R-4.3.2\\bin",
-
-            -- Always open console below current script, a'la RStudio
-            min_editor_width = 72,
-            rconsole_width = 0,
-            rconsole_height = 18,
-            hl_term = false,
-
-            -- Disable all R.nvim default keymaps
-            user_maps_only = true,
-        }
-
-        if vim.env.R_AUTO_START == "true" then
-            opts.auto_start = 1
-            opts.objbr_auto_start = true
-        end
-
-        require("r").setup(opts)
-
-        -- Use tidyverse-style indentation (instead of weird stackoverflow style)
-        -- NB, only applies if indent = { enabled = false } in treesitter config
-        vim.g.r_indent_align_args = 0
-
-        -- Highlight R output using normal colourscheme
-        vim.g.rout_follow_colorscheme = true
-    end,
-    lazy = false,
-
-}
-
+                },
+                min_editor_width = 72,
+                rconsole_width = 78,
+                disable_cmds = {
+                        "RClearConsole",
+                        "RCustomStart",
+                        "RSPlot",
+                        "RSaveClose",
+                    },
+                }
+                -- Check if the environment variable "R_AUTO_START" exists.
+                -- If using fish shell, you could put in your config.fish:
+                -- alias r "R_AUTO_START=true nvim"
+                if vim.env.R_AUTO_START == "true" then
+                    opts.auto_start = 1
+                    opts.objbr_auto_start = true
+                end
+                require("r").setup(opts)
+            end,
+        lazy = false
+    }
+-- local nvim_rprofile_path = (
+--     vim.fn.stdpath("config") .. "/nvim-rprofile.R"
+-- ):gsub("\\", "/")
+--
+-- local r_console_winid = function()
+--     local bufno = require("r.term").get_buf_nr()
+--     if bufno == nil then
+--         return -1
+--     else
+--         return vim.fn.bufwinid(bufno)
+--     end
+-- end
+--
+-- local r_console_open = function() require("r.run").start_R("R") end
+-- local r_console_close = function(winid) vim.fn.win_execute(winid, "q") end
+--
+-- local toggle_r_console = function()
+--     local winid = r_console_winid()
+--     if winid == -1 then
+--         r_console_open()
+--     else
+--         r_console_close(winid)
+--     end
+-- end
+--
+-- local start_r_and_then = function(f)
+--     r_console_open()
+--
+--     -- Should happen very rarely, e.g. if you try to send 
+--     -- a line less than a second after opening a script
+--     if vim.g.R_Nvim_status < 4 then return end
+--
+--     -- Repeatedly check to see if R has started - once yes,
+--     -- send the line
+--     local timer = vim.loop.new_timer()
+--     local i = 0
+--     timer:start(0, 200, vim.schedule_wrap(function()
+--         -- Give up trying to start R after about 10 seconds
+--         if vim.g.R_Nvim_status > 6 or i > 50 then
+--             timer:close()
+--             f()
+--         end
+--         i = i + 1
+--     end))
+-- end
+--
+-- local send_line = function()
+--
+--     -- If R is running, open the console if closed and send 
+--     -- the line
+--     if vim.g.R_Nvim_status > 6 then
+--         if r_console_winid() == -1 then r_console_open() end
+--         require("r.send").line(true)
+--         return
+--     end
+--
+--     start_r_and_then(function() require("r.send").line(true) end)
+-- end
+--
+-- return {
+--     -- "R-nvim/R.nvim",
+--     dir = "~/Repos/R.nvim",
+--     config = function()
+--         -- Create a table with the options to be passed to setup()
+--         local opts = {
+--             R_args = { "--quiet", "--no-save" },
+--             hook = {
+--
+--                 after_R_start = function()
+--                     require("r.send").cmd('source("' .. nvim_rprofile_path .. '")')
+--                 end,
+--
+--                 after_config = function()
+--                     -- This function will be called at the FileType event
+--                     -- of files supported by R.nvim. This is an
+--                     -- opportunity to create mappings local to buffers
+--
+--                     if vim.o.syntax ~= "rbrowser" then
+--                         vim.keymap.set("n", "<Enter>", send_line, { buffer = 0 })
+--                         vim.api.nvim_buf_set_keymap(0, "v", "<Enter>", "<Plug>RSendSelection", {})
+--                     end
+--
+--                     vim.keymap.set("n", "<localleader>rt", toggle_r_console, { buffer = 0 })
+--                     vim.keymap.set("n", "<localleader>rq", "<Plug>RClose", { buffer = 0 })
+--                     vim.keymap.set("n", "<localleader>ro", "<Plug>ROBToggle", { buffer = 0 })
+--
+--                     -- devtools::load_all()
+--                     vim.keymap.set(
+--                         { "n", "i" }, "<localleader>la",
+--                         function()
+--                             start_r_and_then(function()
+--                                 require("r.send").cmd("devtools::load_all()")
+--                             end)
+--                         end,
+--                         { buffer = 0 }
+--                     )
+--
+--                     -- Pipe operator
+--                     -- vim.api.nvim_buf_set_keymap(0, "i", "<C-k>", " |>", {})
+--                 end,
+--             },
+--
+--             -- Note that on macOS, you need to set the option key as the 'meta'
+--             -- key, e.g. in your iterm2 profile, for this to work
+--             assign_map = "<M-->",
+--             pipe = {
+--                 create_keymap = true,
+--                 keymap = "<localleader>m",
+--                 version = "native"
+--             },
+--
+--             -- For some reason gets set to 'Rterm' on windows
+--             R_cmd = "R",
+--             R_app = "R",
+--
+--             -- Windows testing
+--             -- R_path = "C:\\Program Files\\R\\R-4.3.2\\bin",
+--
+--             -- Always open console below current script, a'la RStudio
+--             min_editor_width = 72,
+--             rconsole_width = 0,
+--             rconsole_height = 18,
+--             hl_term = false,
+--
+--             -- Disable all R.nvim default keymaps
+--             user_maps_only = true,
+--         }
+--
+--         if vim.env.R_AUTO_START == "true" then
+--             opts.auto_start = 1
+--             opts.objbr_auto_start = true
+--         end
+--
+--         require("r").setup(opts)
+--
+--         -- Use tidyverse-style indentation (instead of weird stackoverflow style)
+--         -- NB, only applies if indent = { enabled = false } in treesitter config
+--         vim.g.r_indent_align_args = 0
+--
+--         -- Highlight R output using normal colourscheme
+--         vim.g.rout_follow_colorscheme = true
+--     end,
+--     lazy = false,
+--
+-- }
+--
--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,23 +1,44 @@
-return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-    config = function()
-        local config = require("nvim-treesitter.configs")
-        config.setup({
-            auto_install = true,
-            ensure_installed = { "lua", "python", "r", "markdown", "rnoweb" },
-            highlight = { enable = true },
-            indent = { enable = true },
-            -- textobjects = { enable = true },
-            incremental_selection = {
-                enable = true,
-                keymaps = {
-                    init_selection = "tis",
-                    node_incremental = "ni",
-                    scope_incremental = "si",
-                    node_decremental = "nd",
-                },
+return { -- Highlight, edit, and navigate code
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
+    opts = {
+        ensure_installed = {
+            'bash', 'c', 'html', 'lua', 'luadoc', 'markdown', 'python', 'r',
+            'rnoweb', 'vim', 'vimdoc'
+        },
+        -- Autoinstall languages that are not installed
+        auto_install = true,
+        highlight = {
+            enable = true,
+            -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
+            --  If you are experiencing weird indenting issues, add the language to
+            --  the list of additional_vim_regex_highlighting and disabled languages for indent.
+            additional_vim_regex_highlighting = { 'ruby' },
+        },
+        indent = { enable = true, disable = { 'ruby' } },
+        textobjects = { enable = true },
+        incremental_selection = {
+            enable = true,
+            keymaps = {
+                init_selection = "tis",
+                node_incremental = "ni",
+                scope_incremental = "si",
+                node_decremental = "nd",
             },
-        })
+        },
+    },
+    config = function(_, opts)
+        -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
+
+        ---@diagnostic disable-next-line: missing-fields
+        require('nvim-treesitter.configs').setup(opts)
+
+        -- There are additional nvim-treesitter modules that you can use to interact
+        -- with nvim-treesitter. You should go explore a few and see what interests you:
+        --
+        --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
+        --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
+        --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
     end,
 }
+
