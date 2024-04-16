7416debe6e738efc0b076b65665a1b42dfea7234
Jacob Scott <jscott2718@gmail.com>
2024-04-16 10:47:40 +0100

    make completions a bit more fluid

--- nvim/lua/plugins/completions.lua
+++ nvim/lua/plugins/completions.lua
@@ -47,6 +47,40 @@ return { -- Autocompletion
                     luasnip.lsp_expand(args.body)
                 end,
             },
+            --
+            -- 						*'completeopt'* *'cot'*
+            -- 'completeopt' 'cot'	string	(default: "menu,preview")
+            -- 			global
+            -- 	A comma-separated list of options for Insert mode completion
+            -- 	|ins-completion|.  The supported values are:
+            --
+            -- 	   menu	    Use a popup menu to show the possible completions.  The
+            -- 		    menu is only shown when there is more than one match and
+            -- 		    sufficient colors are available.  |ins-completion-menu|
+            --
+            -- 	   menuone  Use the popup menu also when there is only one match.
+            -- 		    Useful when there is additional information about the
+            -- 		    match, e.g., what file it comes from.
+            --
+            -- 	   longest  Only insert the longest common text of the matches.  If
+            -- 		    the menu is displayed you can use CTRL-L to add more
+            -- 		    characters.  Whether case is ignored depends on the kind
+            -- 		    of completion.  For buffer text the 'ignorecase' option is
+            -- 		    used.
+            --
+            -- 	   preview  Show extra information about the currently selected
+            -- 		    completion in the preview window.  Only works in
+            -- 		    combination with "menu" or "menuone".
+            --
+            -- 	  noinsert  Do not insert any text for a match until the user selects
+            -- 		    a match from the menu. Only works in combination with
+            -- 		    "menu" or "menuone". No effect if "longest" is present.
+            --
+            -- 	  noselect  Do not select a match in the menu, force the user to
+            -- 		    select one from the menu. Only works in combination with
+            -- 		    "menu" or "menuone".
+
+
             completion = { completeopt = 'menu,menuone,noinsert' },
 
             -- For an understanding of why these mappings were
@@ -67,8 +101,8 @@ return { -- Autocompletion
                 --  This will auto-import if your LSP supports it.
                 --  This will expand snippets if the LSP sent a snippet.
                 ['<C-y>'] = cmp.mapping.confirm { select = true },
-                ["<tab>"] = cmp.mapping.confirm({ select = true }),
-                ["<cr>"] = cmp.mapping.confirm({ select = false }),
+                ['<tab>'] = cmp.mapping.confirm { select = true },
+                ['<C-c>'] = cmp.mapping.abort(),
 
                 -- Manually trigger a completion from nvim-cmp.
                 --  Generally you don't need this, because nvim-cmp will display
