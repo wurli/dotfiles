a9facb11a609740dd9c28fac5b9d0cd0ddd0448f
Jacob Scott <jscott2718@gmail.com>
2024-05-01 12:00:09 +0100

    make R console aware of width on Windows

--- nvim/nvim-rprofile.R
+++ nvim/nvim-rprofile.R
@@ -1,10 +1,41 @@
-options(cli.num_colors = 256)
-
 local({
   repos <- getOption("repos")
   repos["CRAN"] <- "http://cran.rstudio.com"
   options(repos = repos)
 })
+
+if (.Platform[["OS.type"]] == "Windows") {
+  # Manually set cli to use more terminal colours (e.g. for tibble printing)
+  options(cli.num_colors = 256)
+
+  # Hack `getOption()` to always update the console width, which for some
+  # reason doesn't happen with the default windows R console
+  if (requireNamespace("cli", quietly = TRUE)) {
+    assignInNamespace(
+      "getOption",
+      function(x, default = NULL) {
+        # Check and maybe update console width
+        if (x %in% c("width", "pillar.width")) {
+          options(width = cli::console_width())
+          options(pillar.width = cli::console_width())
+        }
+
+        # Default getOption() implementation from here:
+        if (missing(default)) {
+          .Internal(getOption(x))
+        } else {
+          ans <- .Internal(getOption(x))
+          if (is.null(ans)) {
+            default
+          } else {
+            ans
+          }
+        }
+      }, 
+      ns = "base"
+    )
+  }
+}
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # A possible way to set a faster device for previewing plots if on macOS 
