3eed437b6a668728a5f2d06f2ab5f726764ef666
Jacob Scott <jscott2718@gmail.com>
2024-05-02 18:36:21 +0100

    use cursorline

--- nvim/init.lua
+++ nvim/init.lua
@@ -34,6 +34,7 @@ vim.opt.smartindent = true
 vim.opt.expandtab = true
 vim.opt.colorcolumn = { 80, 120 }
 vim.opt.tabstop = 4
+vim.opt.cursorline = true
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 
