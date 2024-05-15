4e024c9ceb0d3ea3270a454778e9b30790216bcb
Jacob Scott <jscott2718@gmail.com>
2024-05-16 00:12:42 +0100

    remove neo-tree

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -1,5 +1,6 @@
 return {
     "nvim-tree/nvim-tree.lua",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
         require("nvim-tree").setup {}
         local api = require("nvim-tree.api")
