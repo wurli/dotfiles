ab3aaed3abd43e0ebdd909b645a1a27b8f2140c3
Jacob Scott <jscott2718@gmail.com>
2024-05-16 21:49:19 +0100

    update nvim tree and snippet navigation

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -2,7 +2,10 @@ return {
     "nvim-tree/nvim-tree.lua",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
-        require("nvim-tree").setup {}
+        require("nvim-tree").setup {
+            modified = { enable = true },
+            update_focused_file = { enable = true },
+        }
         local api = require("nvim-tree.api")
         vim.keymap.set({ "n", "i" }, "<C-n>", api.tree.toggle)
     end
