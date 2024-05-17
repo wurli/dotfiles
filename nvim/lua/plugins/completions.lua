39efbffc8b85596cec1d1a1209273a1088feabcf
Jacob Scott <jscott2718@gmail.com>
2024-05-17 10:59:20 +0100

    don't overwrite <c-h> in insert mode

--- nvim/lua/plugins/completions.lua
+++ nvim/lua/plugins/completions.lua
@@ -83,12 +83,12 @@ return { -- Autocompletion
                 --
                 -- <c-l> will move you to the right of each of the expansion locations.
                 -- <c-h> is similar, except moving you backwards.
-                ['<C-l>'] = cmp.mapping(function()
+                ['<C-j>'] = cmp.mapping(function()
                     if luasnip.expand_or_locally_jumpable() then
                         luasnip.expand_or_jump()
                     end
                 end, { 'i', 's' }),
-                ['<C-h>'] = cmp.mapping(function()
+                ['<C-k>'] = cmp.mapping(function()
                     if luasnip.locally_jumpable(-1) then
                         luasnip.jump(-1)
                     end
