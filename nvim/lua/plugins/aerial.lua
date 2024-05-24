d9e4113b598fdda961e8712ac403550c024315f4
Jacob Scott <jscott2718@gmail.com>
2024-05-24 09:15:58 +0100

    configure aerial a bit

--- nvim/lua/plugins/aerial.lua
+++ nvim/lua/plugins/aerial.lua
@@ -10,11 +10,12 @@ return {
             layout = {
                 max_width = { 40, 0.2 },
                 min_width = 30,
+                default_direction = "right"
             },
             on_attach = function(bufnr)
                 -- Jump forwards/backwards with '{' and '}'
-                vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
-                vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
+                vim.keymap.set("n", "<leader>{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
+                vim.keymap.set("n", "<leader>}", "<cmd>AerialNext<CR>", { buffer = bufnr })
             end,
         })
         vim.keymap.set("n", "<leader>at", "<cmd>AerialToggle<CR>")
