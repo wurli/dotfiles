9d34cdf1444d2161d76b3142c4e5dca4279cfcea
Jacob Scott <jscott2718@gmail.com>
2024-06-18 18:10:46 +0100

    configure gitsigns a bit more

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -22,6 +22,7 @@ return {
                         -- map('n', '<leader>hR', gitsigns.reset_buffer)
                         map('n', '<leader>hp', gitsigns.preview_hunk)
                         map('n', '<leader>hb', function() gitsigns.blame_line{ full = true } end)
+                        map('n', '<leader>hB', gitsigns.blame)
                         map('n', '<leader>tb', gitsigns.toggle_current_line_blame)
                         map('n', '<leader>hd', gitsigns.diffthis)
                         map('n', '<leader>hD', function() gitsigns.diffthis('~') end)
