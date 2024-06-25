54d4ae7631532bbb02b98e28b46504e30d3a9ea7
Jacob Scott <jscott2718@gmail.com>
2024-06-25 17:05:06 +0100

    disable highlighting colour words

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -27,7 +27,19 @@ return {
         -- Colour hex codes like #000000
         "norcalli/nvim-colorizer.lua",
         config = function()
-            require("colorizer").setup()
+            require("colorizer").setup({ "*" }, {
+                RGB      = true,  -- #RGB hex codes 
+                RRGGBB   = true,  -- #RRGGBB hex codes like #000000
+                names    = false, -- "Name" codes like 'Blue'
+                RRGGBBAA = false, -- #RRGGBBAA hex codes
+                rgb_fn   = false, -- CSS rgb() and rgba() functions
+                hsl_fn   = false, -- CSS hsl() and hsla() functions
+                css      = false, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+                css_fn   = false, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+
+                -- Available modes: foreground, background
+                mode     = 'background', -- Set the display mode.
+            })
         end
     },
 }
