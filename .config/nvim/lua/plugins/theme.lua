f7be0b03758a2d7a9f8566c180666d1b07c81fa7
Jacob Scott <jscott2718@gmail.com>
2024-11-25 12:56:55 +0000

    add indent-blankline.nvim

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -9,6 +9,34 @@ return {
         end
     },
     {
+        "lukas-reineke/indent-blankline.nvim",
+        opts = {
+            indent = {
+                char = "│",
+                tab_char = "│",
+            },
+            scope = { show_start = false, show_end = false },
+            exclude = {
+                filetypes = {
+                    "Trouble",
+                    "alpha",
+                    "dashboard",
+                    "help",
+                    "lazy",
+                    "mason",
+                    "neo-tree",
+                    "notify",
+                    "snacks_notif",
+                    "snacks_terminal",
+                    "snacks_win",
+                    "toggleterm",
+                    "trouble",
+                },
+            },
+        },
+        main = "ibl",
+    },
+    {
         "nvim-lualine/lualine.nvim",
         cond = not vim.g.vscode,
         lazy = false,
