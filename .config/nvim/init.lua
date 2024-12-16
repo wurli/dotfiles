7be2e9fbf949034f13ec4682bbde11e452e57bd6
Jacob Scott <jscott2718@gmail.com>
2024-12-16 10:03:23 +0000

    add some render stuff

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -132,7 +132,58 @@ vim.api.nvim_create_user_command(
     { desc = "Trim trailing whitespace from lines" }
 )
 
+
+
+local command_exists = function(cmd)
+    local res = vim.system(
+        { "which", "-s", cmd },
+        { text = true },
+        function(_) end
+    ):wait()
+
+    return res.code == 0
+end
+
+
+vim.api.nvim_create_user_command(
+    "Preview",
+    function()
+        if not command_exists("quarto") then
+            print("Could not find quarto binary")
+            vim.fn.getchar()
+        end
+
+        local file = vim.api.nvim_buf_get_name(0)
+
+        vim.system(
+            { "quarto", "preview", file },
+            { text = true },
+            function(_) end
+        )
+    end,
+    { desc = "A wrapper for `quarto preview`" }
+)
+
+vim.api.nvim_create_user_command(
+    "Render",
+    function(args)
+        if not command_exists("quarto") then
+            print("Could not find quarto binary")
+            vim.fn.getchar()
+        end
+
+        local file = vim.api.nvim_buf_get_name(0)
+
+        vim.system(
+            { "quarto", "render", file },
+            { text = true },
+            function(_) end
+        )
+    end,
+    { desc = "A wrapper for `quarto render`" }
+)
+
+
 -- Workaround for meta-key limitations in iterm2
 vim.keymap.set({ "i", "c" }, "<M-3>", "#", { noremap = true })
-
 
