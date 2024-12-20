fcd127199719c568fb61e6ac3b9f7b9766620c0f
Jacob Scott <jscott2718@gmail.com>
2024-12-20 17:13:14 +0000

    Add some terminal mappings

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -71,6 +71,9 @@ vim.keymap.set("i", "<C-c>", "<Esc>")
 
 -- Move out of terminal mode
 -- vim.keymap.set("t", "<C-c>", "<C-\\><C-n>", { noremap = false })
+vim.keymap.set("t", "<C-c>", "<C-\\><C-n>")
+vim.keymap.set("t", "<leader>c", "<C-c>")
+
 vim.keymap.set("t", "<C-h>", "<C-\\><C-n><C-w>h")
 vim.keymap.set("t", "<C-j>", "<C-\\><C-n><C-w>j")
 vim.keymap.set("t", "<C-k>", "<C-\\><C-n><C-w>k")
