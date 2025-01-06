af2512c98aca334e9b414e28523aee7f3ce50790
Jacob Scott <jscott2718@gmail.com>
2025-01-06 14:32:36 +0000

    Try using modified rose-pine

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -56,7 +56,44 @@ local rose_pine = {
 	"rose-pine/neovim",
 	name = "rose-pine",
 	config = function()
-        require("rose-pine").setup({})
+        require("rose-pine").setup({
+            palette = {
+                main = {
+                    ----------------
+                    --- Defaults ---
+                    ----------------
+                    -- _nc            = "#16141f",
+                    -- base           = "#191724",
+                    -- surface        = "#1f1d2e",
+                    -- overlay        = "#26233a", -- Current line hightlight, indentation guides
+                    -- muted          = "#6e6a86",
+                    -- subtle         = "#908caa", -- Comments, logical operators
+                    -- text           = "#e0def4",
+                    -- love           = "#eb6f92",
+                    -- gold           = "#f6c177",
+                    -- rose           = "#ebbcba",
+                    -- pine           = "#31748f", -- Variables
+                    -- foam           = "#9ccfd8",
+                    -- iris           = "#c4a7e7",
+                    -- leaf           = "#95b1ac",
+                    -- highlight_low  = "#21202e",
+                    -- highlight_med  = "#403d52",
+                    -- highlight_high = "#524f67",
+                    -- none           = "NONE",
+                    ----------------
+                    --- Adjustments ---
+                    ----------------
+                    overlay = "#322f4d",
+                    pine = "#c3ebfc"
+                }
+            },
+            highlight_groups = {
+                ["@markup.strong"] = { fg = "rose" },
+                ["@markup.italic"] = { fg = "iris" },
+                ["@markup.raw"] = { fg = "leaf" },
+                ["@markup.quote"] = { fg = "subtle", italic = true }
+            }
+        })
 		vim.cmd("colorscheme rose-pine")
 	end
 }
