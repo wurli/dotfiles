c6beece1e85322cd9167482456d63eb6ac3a6cc7
Jacob Scott <jscott2718@gmail.com>
2025-01-06 18:14:44 +0000

    Minor restyle

--- .config/nvim/plugin/options.lua
+++ .config/nvim/plugin/options.lua
@@ -1,23 +1,23 @@
+vim.g.ftplugin_sql_omni_key = "<C-s>"
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
-vim.g.ftplugin_sql_omni_key = "<C-s>"
-
--- Together give 'hybrid' line numbers
-vim.wo.relativenumber = true
-vim.wo.number = true
 
 vim.opt.autoindent = true
-vim.opt.smartindent = true
-vim.opt.expandtab = true
+vim.opt.clipboard = "unnamedplus"
 vim.opt.colorcolumn = { 80, 120 }
-vim.opt.tabstop = 4
 vim.opt.cursorline = true
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.swapfile = false
+vim.opt.expandtab = true
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
+vim.opt.shiftwidth = 4
+vim.opt.smartindent = true
 vim.opt.so = 7
+vim.opt.softtabstop = 4
 vim.opt.splitright = true
-vim.opt.clipboard = "unnamedplus"
+vim.opt.swapfile = false
+vim.opt.tabstop = 4
+
+-- Together give 'hybrid' line numbers
+vim.wo.number = true
+vim.wo.relativenumber = true
 
