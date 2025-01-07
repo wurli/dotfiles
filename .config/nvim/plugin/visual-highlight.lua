376110fe6bcd4c731c60a38e73b70865b6576de3
Jacob Scott <jscott2718@gmail.com>
2025-01-07 23:39:27 +0000

    update visual highlight stuff

--- .config/nvim/plugin/visual-highlight.lua
+++ .config/nvim/plugin/visual-highlight.lua
@@ -10,8 +10,7 @@ local gfind = function(x, pattern, plain)
         init = stop + 1
     end
 
-    local match_line = 1
-    local match_col = 0
+    local match_line, match_col = 1, 0
 
     for _, m in pairs(matches) do
         for _, type in ipairs({ "start", "stop" }) do
@@ -37,45 +36,46 @@ vim.api.nvim_create_autocmd({ "CursorMoved", "ModeChanged" }, {
         local mode = vim.fn.mode()
         if mode ~= "v" and mode ~= "V" then return end
 
-        local selected_text = vim.fn.getregion(
-            vim.fn.getpos("v"),
-            vim.fn.getpos("."),
-            { type = mode }
-        )
+        local start_pos, end_pos = vim.fn.getpos("v"), vim.fn.getpos(".")
+        local selection = vim.fn.getregion(start_pos, end_pos, { type = mode })
 
         local max_lines = 30
         local min_chars = 6
 
-        if #selected_text > max_lines then return end
-        local selected_text_collapsed = table.concat(selected_text, "\n")
+        if #selection > max_lines then return end
+        local selection_collapsed = table.concat(selection, "\n")
 
-        selected_text_collapsed = vim.trim(selected_text_collapsed)
+        selection_collapsed = vim.trim(selection_collapsed)
 
-        if #selected_text_collapsed < min_chars or selected_text_collapsed:find("^%s*$") then
-            return
-        end
+        if #selection_collapsed < min_chars or selection_collapsed:find("^%s*$") then return end
 
-        selected_text_collapsed = selected_text_collapsed:gsub("(%p)", "%%%0")
-        selected_text_collapsed = selected_text_collapsed:gsub("[ \t]+", "%%s+")
+        selection_collapsed = selection_collapsed:gsub("(%p)", "%%%0"):gsub("[ \t]+", "%%s+")
 
-        local win = vim.api.nvim_get_current_win()
-        local first_line = vim.fn.line("w0", win) - #selected_text
-        local last_line  = vim.fn.line("w$", win) + #selected_text
-
+        local first_line   = vim.fn.line("w0", vim.api.nvim_get_current_win()) - #selection
+        local last_line    = vim.fn.line("w$", vim.api.nvim_get_current_win()) + #selection
         local visible_text = vim.api.nvim_buf_get_lines(0, first_line, last_line, false)
-
-        local matches = gfind(visible_text, selected_text_collapsed, false)
+        local matches      = gfind(visible_text, selection_collapsed, false)
 
         for _, m in pairs(matches) do
-            for l = m.start.line, m.stop.line do
-                vim.api.nvim_buf_add_highlight(
-                    0, match_ns, "Search", l + first_line - 1,
-                    l == m.start.line and m.start.col - 1 or 0,
-                    l == m.stop.line and m.stop.col or -1
-                )
+            m.start.line = m.start.line + first_line - 1
+            m.stop.line = m.stop.line + first_line - 1
+        end
+
+        for _, m in pairs(matches) do
+            -- Don't want to re-highlight the selected region
+            local start_ok = m.start.line > end_pos[2] - 1 or (m.start.line == end_pos[2] - 1 and m.start.col > end_pos[3])
+            local stop_ok = m.stop.line < start_pos[2] - 1 or (m.stop.line == start_pos[2] - 1 and m.stop.col < start_pos[3])
+
+            if stop_ok or start_ok then
+                for l = m.start.line, m.stop.line do
+                    vim.api.nvim_buf_add_highlight(
+                        0, match_ns, "Search", l,
+                        l == m.start.line and m.start.col - 1 or 0,
+                        l == m.stop.line and m.stop.col or -1
+                    )
+                end
             end
         end
     end
 })
-
 
