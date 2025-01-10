0f753585ec9d12bf252a49a9aa1daf3aed2b54b0
Jacob Scott <jscott2718@gmail.com>
2025-01-10 10:47:32 +0000

    Refactor coding plugins and add mini.surround

---
+++ .config/nvim/lua/plugins/coding.lua
@@ -0,0 +1,37 @@
+return {
+    {
+        "junegunn/vim-easy-align",
+        config = function()
+            vim.keymap.set({ "n", "v" }, "ga", "<Plug>(EasyAlign)")
+            vim.g.easy_align_delimiters = {
+                ['~'] = { pattern = "\\~" },
+                ['<'] = { pattern = "<\\-" },
+            }
+        end
+    },
+    {
+        'echasnovski/mini.surround',
+        version = false,
+        opts = {}
+    },
+    {
+        'echasnovski/mini.pairs',
+        version = false,
+        opts = {}
+    },
+    {
+        "wurli/split.nvim",
+        -- dir = "~/Repos/split.nvim",
+        opts = {
+            keymap_defaults = {
+                break_placement = function(line_info, opts)
+                    if line_info.filetype == "sql" and not line_info.comment then
+                        return "before_pattern"
+                    end
+                    return "after_pattern"
+                end,
+            },
+        }
+    }
+}
+
