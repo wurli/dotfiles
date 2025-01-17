ec35404ec6ae4bebc1d1ebc2dbcdc58295f36090
Jacob Scott <jscott2718@gmail.com>
2025-01-17 09:50:59 +0000

    update keymaps and add some descriptions

--- .config/nvim/plugin/keymaps.lua
+++ .config/nvim/plugin/keymaps.lua
@@ -1,59 +1,68 @@
+local map = vim.keymap.set
+
 vim.api.nvim_set_keymap("", "\\", "<Nop>", { noremap = true, silent = true })
 
+map("n", "<M-h>", "gT", { desc = "Cycle through tabs" })
+map("n", "<M-l>", "gt", { desc = "Cycle through tabs" })
+
 -- Clear search highlight
-vim.keymap.set("n", "<C-c>", function() vim.v.hlsearch = 0 end)
-vim.keymap.set("n", "<Esc>", function() vim.v.hlsearch = 0 end)
+map("n", "<C-c>", function() vim.v.hlsearch = 0 end, { desc = "Clear search highlights" })
+map("n", "<Esc>", function() vim.v.hlsearch = 0 end, { desc = "Clear search highlights" })
 
 -- Select text after pasting (e.g. for adjusting indentation)
-vim.keymap.set("n", "<leader>v", "`[v`]", {}) -- Select last paste
-vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+map("n", "<leader>v", "`[v`]", { desc = "Select last operated reegion" }) -- Select last paste
+map("n", "<leader>gf", vim.lsp.buf.format, { desc = "LSP format" })
 
 -- For multi-line inserts
-vim.keymap.set("i", "<C-c>", "<Esc>")
+map("i", "<C-c>", "<Esc>")
 
 -- Terminal mode keymaps
-vim.keymap.set("t", "<C-c>", "<C-\\><C-n>")
-vim.keymap.set("t", "<C-.>", "<C-c>")
-vim.keymap.set("t", "<C-h>", "<C-\\><C-n><C-w>h")
-vim.keymap.set("t", "<C-j>", "<C-\\><C-n><C-w>j")
-vim.keymap.set("t", "<C-k>", "<C-\\><C-n><C-w>k")
-vim.keymap.set("t", "<C-l>", "<C-\\><C-n><C-w>l")
+map("t", "<C-c>", "<C-\\><C-n>", { desc = "Terminal exit" })
+map("t", "<C-.>", "<C-c>", { desc = "Terminal clear" })
+map("t", "<C-h>", "<C-\\><C-n><C-w>h", { desc = "Terminal clear" })
+map("t", "<C-j>", "<C-\\><C-n><C-w>j", { desc = "Terminal clear" })
+map("t", "<C-k>", "<C-\\><C-n><C-w>k", { desc = "Terminal clear" })
+map("t", "<C-l>", "<C-\\><C-n><C-w>l", { desc = "Terminal clear" })
 
 -- Source stuff
-vim.keymap.set("n", "<leader>x", "<cmd>.lua<CR>", { desc = "Execute the current line" })
-vim.keymap.set("n", "<leader><leader>x", "<cmd>source %<CR>", { desc = "Execute the current file" })
+map("n", "<leader>x", "<cmd>.lua<CR>", { desc = "Execute the current line" })
+map("n", "<leader><leader>x", "<cmd>source %<CR>", { desc = "Execute the current file" })
 
 -- Reindent on paste; use leader to not indent
-vim.keymap.set({ "n", "v" }, "p", "p`[=`]")
-vim.keymap.set({ "n", "v" }, "P",  "P`[=`]")
-vim.keymap.set({ "n", "v" }, "<leader>p", "p")
-vim.keymap.set({ "n", "v" }, "<leader>P", "P")
+map({ "n", "v" }, "p", "p`[=`]", { desc = "Reindent on paste" })
+map({ "n", "v" }, "P",  "P`[=`]", { desc = "Reindent on paste" })
+map({ "n", "v" }, "<leader>p", "p", { desc = "Normal paste" })
+map({ "n", "v" }, "<leader>P", "P", { desc = "Normal paste" })
 
-vim.keymap.set("n", "gj", function() vim.fn.append(vim.fn.line("."), "") end)
-vim.keymap.set("n", "gk", function() vim.fn.append(vim.fn.line(".") - 1, "") end)
+map("n", "gj", function() vim.fn.append(vim.fn.line("."), "") end, { desc = "Insert blank line below" })
+map("n", "gk", function() vim.fn.append(vim.fn.line(".") - 1, "") end, { desc = "Insert blank line above" })
 
--- Yank into system clipboard
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
-vim.keymap.set("n", "<leader>Y", [["+Y]])
-
--- Delete without adding to buffer
-vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+-- Delete without adding to register
+map({"n", "v"}, "<leader>d", [["_d]], { desc = "Delete into empty register" })
 
 -- These mappings control the size of splits (height/width)
-vim.keymap.set("n", "<M-,>", "<c-w>5<")
-vim.keymap.set("n", "<M-.>", "<c-w>5>")
-vim.keymap.set("n", "<M-;>", "<C-W>-")
-vim.keymap.set("n", "<M-'>", "<C-W>+")
+map("n", "<M-,>", "<c-w>5<", { desc = "Descrease split width" })
+map("n", "<M-.>", "<c-w>5>", { desc = "Increase split width" })
+map("n", "<M-;>", "<C-W>-", { desc = "Decrease split height" })
+map("n", "<M-'>", "<C-W>+",  { desc = "Increase split height" })
 
 -- Move line down
-vim.keymap.set("n", "<M-j>", function()
-    vim.cmd(vim.opt.diff:get() and "normal! ]c]" or "m .+1<CR>==")
-end)
+map(
+    "n", "<M-j>",
+    function()
+        vim.cmd(vim.opt.diff:get() and "normal! ]c]" or "m .+1<CR>==")
+    end,
+    { desc = "Move line down" }
+)
 
 -- Move line up
-vim.keymap.set("n", "<M-k>", function()
-    vim.cmd(vim.opt.diff:get() and "normal! [c]" or "m .-2<CR>==")
-end)
+map(
+    "n", "<M-k>",
+    function()
+        vim.cmd(vim.opt.diff:get() and "normal! [c]" or "m .-2<CR>==")
+    end,
+    { desc = "Move line up" }
+)
 
 -- Workaround for meta-key limitations in terminal emulators
-vim.keymap.set({ "i", "c" }, "<M-3>", "#", { noremap = true })
+map({ "i", "c" }, "<M-3>", "#", { noremap = true, desc = "Insert #" })
