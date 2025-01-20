26b716e0c36664b9b166618e0731e54041c5ba47
Jacob Scott <jscott2718@gmail.com>
2025-01-20 00:36:32 +0000

    remove some old stuff

--- .config/nvim/after/ftplugin/markdown.lua
+++ .config/nvim/after/ftplugin/markdown.lua
@@ -1,28 +1,2 @@
-_G.markdown_indent = function()
-    local ok, parser = pcall(vim.treesitter.get_parser, 0, '')
+vim.opt_local.textwidth = 80
 
-    -- Fall back to autoindent
-    if not ok then return -1 end
-
-    local row, col = vim.fn.line(".") - 1, vim.fn.col(".")
-    local ft = parser:language_for_range({ row, col, row, col + 1 }):lang()
-
-    -- Fall back to autoindent
-    if ft == "" then return -1 end
-
-    local indentexpr = vim.filetype.get_option(ft, "indentexpr"):gsub("%(%)$", "")
-
-    -- Fall back to autoindent
-    if ft == "markdown" or ft == "markdown_inline" or indentexpr == "" then return -1 end
-
-    -- If we're in a code chunk for another language, use the indent settings
-    -- for that language please
-    local shiftwidth   = vim.opt.shiftwidth
-    vim.opt.shiftwidth = vim.filetype.get_option(ft, "shiftwidth")
-    local indent       = vim.fn[indentexpr]()
-    vim.opt.shiftwidth = shiftwidth
-
-    return indent
-end
-
-vim.opt.indentexpr = "v:lua._G.markdown_indent()"
