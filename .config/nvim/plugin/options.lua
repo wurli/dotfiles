6a845e827f114f13b3ed2034b01e3f506240edd1
Jacob Scott <jscott2718@gmail.com>
2025-01-20 23:30:58 +0000

    update ops

--- .config/nvim/plugin/options.lua
+++ .config/nvim/plugin/options.lua
@@ -1,20 +1,16 @@
-vim.g.ftplugin_sql_omni_key = "<C-s>"
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-
-vim.opt.autoindent = true
-vim.opt.clipboard = "unnamedplus"
+vim.opt.autoindent  = true
+vim.opt.clipboard   = "unnamedplus"
 vim.opt.colorcolumn = { 80, 120 }
-vim.opt.cursorline = true
-vim.opt.expandtab = true
-vim.opt.incsearch = true
-vim.opt.shiftwidth = 4
+vim.opt.cursorline  = true
+vim.opt.expandtab   = true
+vim.opt.incsearch   = true
+vim.opt.shiftwidth  = 4
 vim.opt.smartindent = true
-vim.opt.so = 7
+vim.opt.scrolloff   = 7
 vim.opt.softtabstop = 4
-vim.opt.splitright = true
-vim.opt.swapfile = false
-vim.opt.tabstop = 4
+vim.opt.splitright  = true
+vim.opt.swapfile    = false
+vim.opt.tabstop     = 4
 
 -- Together give 'hybrid' line numbers
 vim.wo.number = true
