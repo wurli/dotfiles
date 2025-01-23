5abc8620ffbd660f8cfa58be2a158812855c1059
Jacob Scott <jscott2718@gmail.com>
2025-01-23 20:02:41 +0000

    change some opts

--- .config/nvim/plugin/options.lua
+++ .config/nvim/plugin/options.lua
@@ -3,16 +3,20 @@ vim.opt.clipboard   = "unnamedplus"
 vim.opt.colorcolumn = { 80, 120 }
 vim.opt.cursorline  = true
 vim.opt.expandtab   = true
+vim.opt.inccommand  = "split"
 vim.opt.incsearch   = true
+vim.opt.scrolloff   = 7
 vim.opt.shiftwidth  = 4
 vim.opt.smartindent = true
-vim.opt.scrolloff   = 7
 vim.opt.softtabstop = 4
 vim.opt.splitright  = true
 vim.opt.swapfile    = false
 vim.opt.tabstop     = 4
 
+vim.opt.formatoptions:remove "o"
+
 -- Together give 'hybrid' line numbers
 vim.wo.number = true
 vim.wo.relativenumber = true
+
 
