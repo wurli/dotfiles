b0d904ed527b9f424c753f05807691ce0cc8474d
Jacob Scott <jscott2718@gmail.com>
2025-01-26 18:31:44 +0000

    add otter.nvim

---
+++ .config/nvim/lua/plugins/otter.lua
@@ -0,0 +1,27 @@
+vim.api.nvim_create_autocmd("InsertEnter", {
+    group = vim.api.nvim_create_augroup("otter-autostart", {}),
+    pattern = { "*.md", "*.qmd" },
+    callback = function()
+        local ok, parser = pcall(vim.treesitter.get_parser)
+        if not ok then return end
+
+        local otter      = require("otter")
+        local extensions = require("otter.tools.extensions")
+        local attached   = {}
+
+        local line = vim.fn.line(".") - 1
+        local lang = parser:language_for_range({ line, 0, line, 1 }):lang()
+
+        if extensions[lang] and not vim.tbl_contains(attached, lang) then
+            table.insert(attached, lang)
+            vim.schedule(function() otter.activate({ lang }, true, true) end)
+        end
+    end
+})
+
+
+return {
+    'jmbuhr/otter.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter' },
+    opts = {}
+}
