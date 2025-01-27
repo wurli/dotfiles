f32bd8ea7560fb8d98e2f4a3d8f564c815c03f39
Jacob Scott <jscott2718@gmail.com>
2025-01-27 09:41:47 +0000

    add some quarto settings

--- .config/nvim/after/ftplugin/quarto.lua
+++ .config/nvim/after/ftplugin/quarto.lua
@@ -4,6 +4,8 @@ vim.opt_local.filetype = "markdown"
 -- stops a simple `vim.bo.indentexpr = ""` working
 vim.api.nvim_create_autocmd("BufEnter", {
     group = vim.api.nvim_create_augroup("quarto-indent", {}),
-    callback = function() vim.bo.indentexpr = "" end
+    pattern = "*.qmd",
+    callback = function()
+        vim.bo.indentexpr = "v:lua.require('contextindent').context_indent('')"
+    end
 })
-
