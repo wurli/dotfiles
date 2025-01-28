0780ff808e96fa859041639d1ed3cbae93027e06
Jacob Scott <jscott2718@gmail.com>
2025-01-28 08:06:10 +0000

    add markdown-preview.nvim

--- .config/nvim/plugin/commands.lua
+++ .config/nvim/plugin/commands.lua
@@ -1,48 +1,48 @@
-local system_command_exists = function(cmd)
-    local res = vim.system(
-        { "which", "-s", cmd },
-        { text = true },
-        function(_) end
-    ):wait()
-
-    return res.code == 0
-end
-
-vim.api.nvim_create_user_command(
-    "Preview",
-    function()
-        if not system_command_exists("quarto") then
-            print("Could not find quarto binary")
-            vim.fn.getchar()
-        end
-
-        local file = vim.api.nvim_buf_get_name(0)
-
-        vim.system(
-            { "quarto", "preview", file },
-            { text = true },
-            function(_) end
-        )
-    end,
-    { desc = "A wrapper for `quarto preview`" }
-)
-
-vim.api.nvim_create_user_command(
-    "Render",
-    function()
-        if not system_command_exists("quarto") then
-            print("Could not find quarto binary")
-            vim.fn.getchar()
-        end
-
-        local file = vim.api.nvim_buf_get_name(0)
-
-        vim.system(
-            { "quarto", "render", file },
-            { text = true },
-            function(_) end
-        )
-    end,
-    { desc = "A wrapper for `quarto render`" }
-)
-
+-- local system_command_exists = function(cmd)
+--     local res = vim.system(
+--         { "which", "-s", cmd },
+--         { text = true },
+--         function(_) end
+--     ):wait()
+--
+--     return res.code == 0
+-- end
+--
+-- vim.api.nvim_create_user_command(
+--     "Preview",
+--     function()
+--         if not system_command_exists("quarto") then
+--             print("Could not find quarto binary")
+--             vim.fn.getchar()
+--         end
+--
+--         local file = vim.api.nvim_buf_get_name(0)
+--
+--         vim.system(
+--             { "quarto", "preview", file },
+--             { text = true },
+--             function(_) end
+--         )
+--     end,
+--     { desc = "A wrapper for `quarto preview`" }
+-- )
+--
+-- vim.api.nvim_create_user_command(
+--     "Render",
+--     function()
+--         if not system_command_exists("quarto") then
+--             print("Could not find quarto binary")
+--             vim.fn.getchar()
+--         end
+--
+--         local file = vim.api.nvim_buf_get_name(0)
+--
+--         vim.system(
+--             { "quarto", "render", file },
+--             { text = true },
+--             function(_) end
+--         )
+--     end,
+--     { desc = "A wrapper for `quarto render`" }
+-- )
+--
