d4269516d823239e1a5e80b453159e7ff3935455
Jacob Scott <jscott2718@gmail.com>
2025-02-10 10:29:05 +0000

    update wezterm config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -3,16 +3,21 @@ local wezterm = require "wezterm"
 wezterm.on("toggle-opacity", function(window, pane)
     local overrides = window:get_config_overrides() or {}
     if not overrides.window_background_opacity then
-        overrides.text_background_opacity = 0.5
-        overrides.window_background_opacity = 0.5
+        overrides.text_background_opacity = 0.4
+        overrides.window_background_opacity = 0.4
+        overrides.macos_window_background_blur = 0
     else
         overrides.window_background_opacity = nil
         overrides.text_background_opacity = nil
+        overrides.macos_window_background_blur = nil
     end
     window:set_config_overrides(overrides)
 end)
 
 local config = {
+    window_background_opacity = 0.75,
+    text_background_opacity = 0.75,
+    macos_window_background_blur = 40,
     keys = {
         {
             key = "O",
@@ -20,6 +25,13 @@ local config = {
             action = wezterm.action.EmitEvent "toggle-opacity",
         },
     },
+    font = wezterm.font_with_fallback({
+        "FiraMono Nerd Font Mono",
+        "JetBrains Mono"
+    }),
+    font_size = 14
 }
 
 return config
+
+
