976e832c0929a500a954cfd15030e818266ff527
Jacob Scott <jscott2718@gmail.com>
2025-06-19 11:42:34 +0100

    disable treesitter for csv

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -13,6 +13,7 @@ return { -- Highlight, edit, and navigate code
         auto_install = true,
         highlight = {
             enable = true,
+            disable = { "csv" },
             -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
             --  If you are experiencing weird indenting issues, add the language to
             --  the list of additional_vim_regex_highlighting and disabled languages for indent.
