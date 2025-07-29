67d5e1805f142431fc8060f83e195ab01bc37d4c
Jacob Scott <jscott2718@gmail.com>
2025-07-29 18:32:22 +0100

    feat: properly detect snacks in lsp

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -10,6 +10,7 @@ return {
             library = {
                 -- Load luvit types when the `vim.uv` word is found
                 { path = 'luvit-meta/library', words = { 'vim%.uv' } },
+                { path = 'snacks.nvim', words = { 'Snacks' } },
             },
         },
     },
