c8d7b5c274fc92794cc23b1c037d3efaca3371b0
Jacob Scott <jscott2718@gmail.com>
2025-08-12 10:36:11 +0100

    chore: add a harpoon mapping to add current file within quick menu

--- .config/nvim/lua/plugins/harpoon.lua
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -7,7 +7,39 @@ return {
         local harpoon = require("harpoon")
         harpoon:setup()
 
-        vim.keymap.set("n", "<leader>ha", function() harpoon:list():add() end)
+        local cur_file = nil
+
+        vim.keymap.set(
+            "n",
+            "<leader><leader>h",
+            function()
+                if vim.bo.ft ~= "harpoon" then
+                    cur_file = vim.fn.expand("%")
+                end
+                harpoon.ui:toggle_quick_menu(harpoon:list())
+            end,
+            { desc = "Open harpoon UI" }
+        )
+
+        vim.keymap.set("n", "<leader>ha", function()
+            if vim.bo.ft ~= "harpoon" then
+                harpoon:list():add()
+                return
+            end
+
+            if cur_file then
+                local lnum, cnum = unpack(vim.api.nvim_win_get_cursor(0))
+                local all_lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+                for i, line in ipairs(all_lines) do
+                    if line == cur_file then
+                        vim.api.nvim_buf_set_lines(0, i - 1, i, false, {})
+                    end
+                end
+
+                vim.api.nvim_buf_set_lines(0, lnum - 1, lnum - 1, false, { cur_file })
+                vim.api.nvim_win_set_cursor(0, { lnum, cnum })
+            end
+        end)
 
         vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
         vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end)
@@ -16,43 +48,8 @@ return {
         vim.keymap.set("n", "<leader>5", function() harpoon:list():select(5) end)
 
         -- Toggle previous & next buffers stored within Harpoon list
-        vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
-        vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
-
-        vim.keymap.set(
-            "n",
-            "<leader><leader>h",
-            function() harpoon.ui:toggle_quick_menu(harpoon:list()) end,
-            { desc = "Open harpoon UI" }
-        )
-
-        -- basic telescope configuration
-        local conf = require("telescope.config").values
-
-        ---@param harpoon_files HarpoonList
-        local function toggle_telescope(harpoon_files)
-            local file_paths = {}
-            for _, item in ipairs(harpoon_files.items) do
-                table.insert(file_paths, item.value)
-            end
-
-            require("telescope.pickers")
-                .new({}, {
-                    prompt_title = "Harpoon",
-                    finder = require("telescope.finders").new_table({
-                        results = file_paths,
-                    }),
-                    previewer = conf.file_previewer({}),
-                    sorter = conf.generic_sorter({}),
-                })
-                :find()
-        end
-
-        vim.keymap.set(
-            "n",
-            "<leader>hf",
-            function() toggle_telescope(harpoon:list()) end,
-            { desc = "Open harpoon telescope" }
-        )
+        vim.keymap.set("n", "<M-p>", function() harpoon:list():prev() end)
+        vim.keymap.set("n", "<M-n>", function() harpoon:list():next() end)
     end,
 }
+
