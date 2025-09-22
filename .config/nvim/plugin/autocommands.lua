15eab99af79da7497548971e48e965614e441962
Jacob Scott <jscott2718@gmail.com>
2025-09-22 20:39:49 +0100

    fix: don't set (relative)number globally

--- .config/nvim/plugin/autocommands.lua
+++ .config/nvim/plugin/autocommands.lua
@@ -5,8 +5,8 @@ end
 vim.api.nvim_create_autocmd("TermOpen", {
     group = vim.api.nvim_create_augroup("custom-term-open", { clear = true }),
     callback = function()
-        vim.opt.number = false
-        vim.opt.relativenumber = false
+        vim.wo.number = false
+        vim.wo.relativenumber = false
     end
 })
 
