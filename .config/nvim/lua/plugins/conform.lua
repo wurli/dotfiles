6a4da724a1c89a437ccf92260bdaa437359e8a67
Jacob Scott <jscott2718@gmail.com>
2025-11-24 12:54:41 +0000

    fix: conform ruff setting

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -3,7 +3,7 @@ return {
 	opts = {
 		formatters_by_ft = {
 			lua = { "stylua" },
-			python = { "ruff" },
+			python = { "ruff_format" },
 			rust = { "rustfmt", lsp_format = "fallback" },
 			r = { "air", lsp_format = "fallback" },
 		},
