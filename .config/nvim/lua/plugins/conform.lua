9f73b87f0d491b98012afd7f7c6579e9dea463e8
Jacob Scott <jscott2718@gmail.com>
2025-11-24 13:01:11 +0000

    chore: update conform settings

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -1,11 +1,16 @@
 return {
 	"stevearc/conform.nvim",
+	lazy = false,
 	opts = {
 		formatters_by_ft = {
 			lua = { "stylua" },
 			python = { "ruff_format" },
 			rust = { "rustfmt", lsp_format = "fallback" },
 			r = { "air", lsp_format = "fallback" },
+		},
+		format_on_save = {
+			lsp_format = "fallback",
+			timeout_ms = 500,
 		},
 	},
 	keys = {
