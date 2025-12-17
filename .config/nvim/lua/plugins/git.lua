6c17e1596635afb6b0445b1654180c4829c5b826
Jacob Scott <jscott2718@gmail.com>
2025-12-17 14:48:41 +0000

    move to fugitive

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -1,147 +1,183 @@
--- Workaround for https://github.com/NeogitOrg/neogit/issues/1696
-vim.api.nvim_create_autocmd("User", {
-    pattern = "NeogitCommitComplete",
-    callback = function() vim.schedule(vim.cmd.tabprevious) end
-})
+-- -- Workaround for https://github.com/NeogitOrg/neogit/issues/1696
+-- vim.api.nvim_create_autocmd("User", {
+-- 	pattern = "NeogitCommitComplete",
+-- 	callback = function()
+-- 		vim.schedule(vim.cmd.tabprevious)
+-- 	end,
+-- })
+--
+-- local action = function(x)
+-- 	return function()
+-- 		require("diffview.actions")[x]()
+-- 	end
+-- end
 
-local action = function(x)
-    return function() require("diffview.actions")[x]() end
-end
+vim.keymap.set("n", "<leader>lg", "<cmd>vert Git<cr>", { desc = "Git Fugitive" })
 
 return {
-    {
-        "tpope/vim-fugitive",
-        cond = not vim.g.vscode,
-    },
-    {
-        "NeogitOrg/neogit",
-        -- "wurli/neogit",
-        -- branch = "float-highlights",
-        cond = not vim.g.vscode,
-        dependencies = {
-            "nvim-lua/plenary.nvim", -- required
-            "nvim-telescope/telescope.nvim",
-            {
-                "sindrets/diffview.nvim", -- optional - Diff integration
-                opts = {
-                    enhanced_diff_hl = true,
-                    keymaps = {
-                        view = {
-                            { "n", "<c-n>", action("toggle_files"),      { desc = "Toggle the file panel." } },
-                            { "n", "<c-j>", action("select_next_entry"), { desc = "Open the diff for the next file" } },
-                            { "n", "<c-k>", action("select_prev_entry"), { desc = "Open the diff for the previous file" } },
-                            { "n", "q",     "<cmd>tabclose<cr>",         { desc = "Close the diff view" } },
-                        },
-                        file_panel = {
-                            { "n", "<c-n>", action("toggle_files"),      { desc = "Toggle the file panel." } },
-                            { "n", "<c-j>", action("select_next_entry"), { desc = "Open the diff for the next file" } },
-                            { "n", "<c-k>", action("select_prev_entry"), { desc = "Open the diff for the previous file" } },
-                            { "n", "q",     "<cmd>tabclose<cr>",         { desc = "Close the diff view" } },
-                        }
-                    }
-                },
-            },
-        },
-        opts = {
-            kind = "floating",
-            floating = {
-                width = 0.85,
-                height = 0.85
-            },
-            integrations = {
-                snacks = true,
-                telescope = false,
-            },
-            telescope_sorter = function()
-                return require("telescope.sorters").get_fzy_sorter()
-            end
-        },
-        keys = {
-            { "<leader>lg", "<cmd>Neogit<cr>", desc = "Neogit" },
-        },
-    },
-    -- {
-    --     -- Not strictly necessary but does make things nicer overall
-    --     "echasnovski/mini.diff",
-    --     opts = {
-    --         view = {
-    --             style = "sign",
-    --             signs = { add = "┃", change = "┇", delete = "║" }
-    --         },
-    --         mappings = {
-    --             -- Apply hunks inside a visual/operator region
-    --             apply = "gh",
-    --
-    --             -- Reset hunks inside a visual/operator region
-    --             reset = "gH",
-    --
-    --             -- Hunk range textobject to be used inside operator
-    --             -- Works also in Visual mode if mapping differs from apply and reset
-    --             textobject = "gh",
-    --
-    --             -- Go to hunk range in corresponding direction
-    --             goto_first = "[H",
-    --             goto_prev = "[h",
-    --             goto_next = "]h",
-    --             goto_last = "]H",
-    --         },
-    --         options = {
-    --             algorithm = "patience",
-    --         }
-    --     },
-    -- },
-    {
-        "lewis6991/gitsigns.nvim",
-        cond = not vim.g.vscode,
-        config = function()
-            require("gitsigns").setup {
-                diff_opts = {
-                    algorithm = "patience",
-                },
-                signs = {
-                    add          = { text = "┃" },
-                    change       = { text = "┇" },
-                    delete       = { text = "║" },
-                    topdelete    = { text = "║" },
-                    changedelete = { text = "║" },
-                    untracked    = { text = "·" },
-                },
-                signs_staged = {
-                    add          = { text = "┃" },
-                    change       = { text = "┇" },
-                    delete       = { text = "║" },
-                    topdelete    = { text = "║" },
-                    changedelete = { text = "║" },
-                    untracked    = { text = "·" },
-                },
-                on_attach = function(bufnr)
-                    local gitsigns = require("gitsigns")
+	{
+		"tpope/vim-fugitive",
+		cond = not vim.g.vscode,
+	},
+	{
+		"esmuellert/vscode-diff.nvim",
+		branch = "next",
+		dependencies = { "MunifTanjim/nui.nvim" },
+		config = function()
+			require("vscode-diff").setup({
+				keymaps = {
+					view = {
+						toggle_explorer = "<c-n>",
+					},
+				},
+			})
 
-                    local function map(mode, l, r, desc)
-                        vim.keymap.set(mode, l, r, { buffer = bufnr, desc = desc })
-                    end
+			-- _G.vscode_diffexpr = function()
+			-- 	vim.print({
+			-- 		fname_in = vim.v.fname_in,
+			-- 		fname_new = vim.v.fname_new,
+			-- 		fname_out = vim.v.fname_out,
+			-- 	})
+			-- 	vim.schedule(function()
+			-- 		vim.cmd(("CodeDiff file %s %s"):format(vim.v.fname_in, vim.v.fname_new))
+			-- 	end)
+			-- end
+			-- vim.o.diffexpr = "v:lua._G.vscode_diffexpr()"
+		end,
+	},
+	-- {
+	--     "NeogitOrg/neogit",
+	--     -- "wurli/neogit",
+	--     -- branch = "float-highlights",
+	--     cond = not vim.g.vscode,
+	--     dependencies = {
+	--         "nvim-lua/plenary.nvim", -- required
+	--         "nvim-telescope/telescope.nvim",
+	--         {
+	--             "sindrets/diffview.nvim", -- optional - Diff integration
+	--             opts = {
+	--                 enhanced_diff_hl = true,
+	--                 keymaps = {
+	--                     view = {
+	--                         { "n", "<c-n>", action("toggle_files"),      { desc = "Toggle the file panel." } },
+	--                         { "n", "<c-j>", action("select_next_entry"), { desc = "Open the diff for the next file" } },
+	--                         { "n", "<c-k>", action("select_prev_entry"), { desc = "Open the diff for the previous file" } },
+	--                         { "n", "q",     "<cmd>tabclose<cr>",         { desc = "Close the diff view" } },
+	--                     },
+	--                     file_panel = {
+	--                         { "n", "<c-n>", action("toggle_files"),      { desc = "Toggle the file panel." } },
+	--                         { "n", "<c-j>", action("select_next_entry"), { desc = "Open the diff for the next file" } },
+	--                         { "n", "<c-k>", action("select_prev_entry"), { desc = "Open the diff for the previous file" } },
+	--                         { "n", "q",     "<cmd>tabclose<cr>",         { desc = "Close the diff view" } },
+	--                     }
+	--                 }
+	--             },
+	--         },
+	--     },
+	--     opts = {
+	--         kind = "floating",
+	--         floating = {
+	--             width = 0.85,
+	--             height = 0.85
+	--         },
+	--         integrations = {
+	--             snacks = true,
+	--             telescope = false,
+	--         },
+	--         telescope_sorter = function()
+	--             return require("telescope.sorters").get_fzy_sorter()
+	--         end
+	--     },
+	--     keys = {
+	--         { "<leader>lg", "<cmd>Neogit<cr>", desc = "Neogit" },
+	--     },
+	-- },
+	-- {
+	--     -- Not strictly necessary but does make things nicer overall
+	--     "echasnovski/mini.diff",
+	--     opts = {
+	--         view = {
+	--             style = "sign",
+	--             signs = { add = "┃", change = "┇", delete = "║" }
+	--         },
+	--         mappings = {
+	--             -- Apply hunks inside a visual/operator region
+	--             apply = "gh",
+	--
+	--             -- Reset hunks inside a visual/operator region
+	--             reset = "gH",
+	--
+	--             -- Hunk range textobject to be used inside operator
+	--             -- Works also in Visual mode if mapping differs from apply and reset
+	--             textobject = "gh",
+	--
+	--             -- Go to hunk range in corresponding direction
+	--             goto_first = "[H",
+	--             goto_prev = "[h",
+	--             goto_next = "]h",
+	--             goto_last = "]H",
+	--         },
+	--         options = {
+	--             algorithm = "patience",
+	--         }
+	--     },
+	-- },
+	{
+		"lewis6991/gitsigns.nvim",
+		cond = not vim.g.vscode,
+		config = function()
+			require("gitsigns").setup({
+				diff_opts = {
+					algorithm = "patience",
+				},
+				signs = {
+					add = { text = "┃" },
+					change = { text = "┇" },
+					delete = { text = "║" },
+					topdelete = { text = "║" },
+					changedelete = { text = "║" },
+					untracked = { text = "·" },
+				},
+				signs_staged = {
+					add = { text = "┃" },
+					change = { text = "┇" },
+					delete = { text = "║" },
+					topdelete = { text = "║" },
+					changedelete = { text = "║" },
+					untracked = { text = "·" },
+				},
+				on_attach = function(bufnr)
+					local gitsigns = require("gitsigns")
 
-                    -- Actions
-                    -- map("v", "<leader>hs", function() gitsigns.stage_hunk {vim.fn.line("."), vim.fn.line("v")} end)
-                    -- map("v", "<leader>hr", function() gitsigns.reset_hunk {vim.fn.line("."), vim.fn.line("v")} end)
-                    -- map("n", "<leader>hu", gitsigns.undo_stage_hunk)
-                    map("n", "<leader>hp", gitsigns.preview_hunk, "Git Preview Hunk")
-                    map("n", "<leader>hb", function() gitsigns.blame_line { full = true } end, "Git Blame Line")
-                    map("n", "<leader>hB", gitsigns.blame, "Git Blame")
-                    map("n", "<leader>tb", gitsigns.toggle_current_line_blame, "Git Toggle Blame")
-                    map("n", "<leader>hd", gitsigns.diffthis, "Git Diff Against Index")
-                    map("n", "<leader>hD", function() gitsigns.diffthis("~") end, "Git Diff Against HEAD")
-                    map("n", "<leader>td", gitsigns.preview_hunk_inline, "Git Preview Hunk")
-                    map("n", "<leader>hr", gitsigns.reset_hunk, "Git Reset Hunk")
-                    map("n", "<leader>hs", gitsigns.stage_hunk, "Git Stage Hunk")
-                    map("n", "<leader>hr", gitsigns.reset_hunk, "Git Reset Hunk")
-                    map("n", "<leader>hS", gitsigns.stage_buffer, "Git Stage Buffer")
-                    map("n", "<leader>hR", gitsigns.reset_buffer, "Git Reset Buffer")
+					local function map(mode, l, r, desc)
+						vim.keymap.set(mode, l, r, { buffer = bufnr, desc = desc })
+					end
 
-                    -- Text object
-                    map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>")
-                end
-            }
-        end
-    },
+					-- Actions
+					-- map("v", "<leader>hs", function() gitsigns.stage_hunk {vim.fn.line("."), vim.fn.line("v")} end)
+					-- map("v", "<leader>hr", function() gitsigns.reset_hunk {vim.fn.line("."), vim.fn.line("v")} end)
+					-- map("n", "<leader>hu", gitsigns.undo_stage_hunk)
+					map("n", "<leader>hp", gitsigns.preview_hunk, "Git Preview Hunk")
+					map("n", "<leader>hb", function()
+						gitsigns.blame_line({ full = true })
+					end, "Git Blame Line")
+					map("n", "<leader>hB", gitsigns.blame, "Git Blame")
+					map("n", "<leader>tb", gitsigns.toggle_current_line_blame, "Git Toggle Blame")
+					map("n", "<leader>hd", gitsigns.diffthis, "Git Diff Against Index")
+					map("n", "<leader>hD", function()
+						gitsigns.diffthis("~")
+					end, "Git Diff Against HEAD")
+					map("n", "<leader>td", gitsigns.preview_hunk_inline, "Git Preview Hunk")
+					map("n", "<leader>hr", gitsigns.reset_hunk, "Git Reset Hunk")
+					map("n", "<leader>hs", gitsigns.stage_hunk, "Git Stage Hunk")
+					map("n", "<leader>hr", gitsigns.reset_hunk, "Git Reset Hunk")
+					map("n", "<leader>hS", gitsigns.stage_buffer, "Git Stage Buffer")
+					map("n", "<leader>hR", gitsigns.reset_buffer, "Git Reset Buffer")
+
+					-- Text object
+					map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>")
+				end,
+			})
+		end,
+	},
 }
