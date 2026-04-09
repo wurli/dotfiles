b35b1e97f8d3b92d3cacef95458aef8add462736
Jacob Scott <jscott2718@gmail.com>
2026-04-09 23:23:51 +0100

    chore: restore nvim-colorizer (for now)

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -152,4 +152,27 @@ return {
 		"dimtion/guttermarks.nvim",
 		event = "VeryLazy",
 	},
+
+	{
+		-- Colour hex codes like #000000
+		-- TODO: find a replacement; this seems to be abandoned (and now raises
+		-- a warning about tbl_flatten)
+		"norcalli/nvim-colorizer.lua",
+		cond = not vim.g.vscode,
+		config = function()
+			require("colorizer").setup({ "*" }, {
+				RGB = true, -- #RGB hex codes
+				RRGGBB = true, -- #RRGGBB hex codes like #000000
+				names = false, -- "Name" codes like "Blue"
+				RRGGBBAA = true, -- #RRGGBBAA hex codes
+				rgb_fn = false, -- CSS rgb() and rgba() functions
+				hsl_fn = false, -- CSS hsl() and hsla() functions
+				css = false, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+				css_fn = false, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+
+				-- Available modes: foreground, background
+				mode = "background", -- Set the display mode.
+			})
+		end,
+	},
 }
