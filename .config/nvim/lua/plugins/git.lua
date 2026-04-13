d66585d0a78a3999be191976082ef7725b1758c5
Jacob Scott <jscott2718@gmail.com>
2026-04-13 08:15:09 +0100

    feat: update CodeDiff keymaps

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -68,8 +68,8 @@ return {
 					view = {
 						quit = "q", -- Close diff tab
 						toggle_explorer = "<c-n>", -- Toggle explorer visibility (explorer mode only)
-						next_hunk = "]c", -- Jump to next change
-						prev_hunk = "[c", -- Jump to previous change
+						next_hunk = "]h", -- Jump to next change
+						prev_hunk = "[h", -- Jump to previous change
 						next_file = "]f", -- Next file in explorer mode
 						prev_file = "[f", -- Previous file in explorer mode
 						diff_get = "do", -- Get change from other buffer (like vimdiff)
