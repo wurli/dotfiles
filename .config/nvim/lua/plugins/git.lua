75942115dedefccefbab7c778c9c502a6c24c2da
Jacob Scott <jscott2718@gmail.com>
2026-07-14 11:34:32 +0100

    feat: update gitsigns after fugitive

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -49,6 +49,14 @@ vim.api.nvim_create_autocmd("User", {
 	command = "tabclose",
 })
 
+vim.api.nvim_create_autocmd("User", {
+	pattern = "FugitiveChanged",
+	group = vim.api.nvim_create_augroup("custom-fugitive", { clear = true }),
+	callback = function()
+		require("gitsigns").refresh()
+	end,
+})
+
 return {
 	{
 		"tpope/vim-fugitive",
