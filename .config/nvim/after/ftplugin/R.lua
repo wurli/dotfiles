19809513129f76b822d5557bf31e9de8f4b78197
Jacob Scott <jscott2718@gmail.com>
2026-08-14 16:41:56 +0100

    feat: update R config to match air defaults

--- .config/nvim/after/ftplugin/R.lua
+++ .config/nvim/after/ftplugin/R.lua
@@ -1,3 +1,5 @@
-vim.opt_local.tabstop = 2
-vim.opt_local.softtabstop = 2
-vim.opt_local.shiftwidth = 2
+-- These match air's default formatting
+vim.bo.tabstop = 2
+vim.bo.softtabstop = 2
+vim.bo.shiftwidth = 2
+vim.bo.expandtab = true
